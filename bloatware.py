3579da00ed449b78438dafc4189cfba12aeddce3
loldorian <not.so.funny04@gmail.com>
2024-11-05 05:53:13 +0200

    Create bloatware.py

---
+++ bloatware.py
@@ -0,0 +1,416 @@
+"""
+bro ts is so stupid im js so bored :sob:
+"""
+
+import discord
+from discord.ext import commands
+
+token = ""
+
+dorian = commands.Bot(command_prefix="d?", intents=discord.Intents.all())
+
+@dorian.event
+async def on_ready():
+  print("kniggas archangels")
+
+@dorian.command()
+async def bloat1(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat2(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat3(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat4(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat5(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat6(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat7(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat8(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat9(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat10(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat11(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat12(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat13(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat14(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat15(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat16(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat17(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat18(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat19(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat20(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat21(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat22(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat23(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat24(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat25(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat26(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat27(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat28(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat29(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat30(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat31(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat32(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat33(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat34(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat35(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat36(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat37(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat38(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat39(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat40(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat41(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat42(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat43(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat44(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat45(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat46(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat47(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat48(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat49(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat50(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat51(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat52(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat53(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat54(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat55(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat56(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat57(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat58(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat59(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat60(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat61(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat62(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat63(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat64(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat65(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat66(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat67(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat68(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat69(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat70(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat71(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat72(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat73(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat74(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat75(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat76(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat77(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat78(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat79(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat80(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat81(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat82(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat83(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat84(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat85(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat86(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat87(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat88(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat89(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat90(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat91(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat92(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat93(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat94(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat95(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat96(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat97(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat98(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat99(ctx):
+    await ctx.send("nigr")
+
+@dorian.command()
+async def bloat100(ctx):
+    await ctx.send("nigr")
+
+dorian.run(token)
